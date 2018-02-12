9d48ca25dd4d3b3e7768e88cc6c813a80d4f1fb1
Lukas Balles <lukas.balles@gmail.com>
2018-02-12 16:27:12 +0100

    Add setup file, as well as license and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='msvag',
+      version='1.0',
+      description='TensorFlow implementation of (Momentum) Variance-Adapted Stochastic Gradient',
+      url='https://github.com/lballes/msvag',
+      author='Lukas Balles, MPI-IS Tuebingen, Probabilistic Numerics Group',
+      author_email='lballes@tue.mpg.de',
+      license='Apache 2.0',
+      packages=['msvag'],
+      install_requires=[],
+      zip_safe=False)
